e06a2cb543d515f7283fe815b71fe24e6e15c2a6
Andrew <jadedynastymail.ru@mail.ru>
2024-03-19 19:15:59 +0300

    add alias support

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -3,15 +3,40 @@
 const rule = require("../../../lib/rules/path-checker"),
   RuleTester = require("eslint").RuleTester;
 
+// import { addCommentFormActions, addCommentFormReducer } from '@/entities/AddCommentForm/model/slice/addCommentFormSlice'
 
-const ruleTester = new RuleTester();
-ruleTester.run("path-checker", rule, {
-  valid: [],
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+});
+ruleTester.run('path-checker', rule, {
+  valid: [
+    {
+      filename: 'C:\\Users\\asd\\Desktop\\f1\\f2\\src\\entities\\AddCommentForm',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slice/addCommentFormSlice'",
+      errors: [],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
+    }
+  ],
 
   invalid: [
     {
-      code: "none",
-      errors: [{ message: "", type: "" }],
+      filename: 'C:\\Users\\asd\\Desktop\\f1\\f2\\src\\entities\\AddCommentForm',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/AddCommentForm/model/slice/addCommentFormSlice'",
+      errors: [{message: 'Within a slice, all paths must be relative'}],
     },
+    {
+      filename: 'C:\\Users\\asd\\Desktop\\f1\\f2\\src\\entities\\AddCommentForm',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/AddCommentForm/model/slice/addCommentFormSlice'",
+      errors: [{ message: 'Within a slice, all paths must be relative' }],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
+    }
   ],
 });
